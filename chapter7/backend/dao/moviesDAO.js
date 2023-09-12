21d1ee188bbe328307865b634931c03dd9f16239
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-09-12 09:04:06 +0200

    update

--- chapter7/backend/dao/moviesDAO.js
+++ chapter7/backend/dao/moviesDAO.js
@@ -1,41 +1,42 @@
 import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
 
-let movies 
+let movies
 
-export default class MoviesDAO{ 
-    static async injectDB(conn){ 
-        if(movies){ 
+export default class MoviesDAO {
+    static async injectDB(conn) {
+        if (movies) {
             return
         }
-        try{ 
+        try {
             movies = await conn.db(process.env.MOVIEREVIEWS_NS)
-					.collection('movies')
-        } 
-        catch(e){
+                .collection('movies')
+        }
+        catch (e) {
             console.error(`unable to connect in MoviesDAO: ${e}`)
         }
     }
 
-    static async getMovieById(id){        
-        try{                                                 
+    static async getMovieById(id) {
+        try {
             return await movies.aggregate([
                 {
                     $match: {
                         _id: new ObjectId(id),
                     }
-                }    ,
-                { $lookup:
+                },
+                {
+                    $lookup:
                     {
                         from: 'reviews',
                         localField: '_id',
                         foreignField: 'movie_id',
                         as: 'reviews',
                     }
-                }       
-            ]).next()            
+                }
+            ]).next()
         }
-        catch(e){
+        catch (e) {
             console.error(`something went wrong in getMovieById: ${e}`)
             throw e
         }
@@ -46,39 +47,39 @@ export default class MoviesDAO{
         filters = null,
         page = 0,
         moviesPerPage = 20, // will only get 20 movies at once
-    } = {}){
-        let query 
-        if(filters){ 
-            if("title" in filters){ 
-                query = { $text: { $search: filters['title']}}
-            }else if("rated" in filters){ 
-                query = { "rated": { $eq: filters['rated']}} 
-            }                                
+    } = {}) {
+        let query
+        if (filters) {
+            if ("title" in filters) {
+                query = { $text: { $search: filters['title'] } }
+            } else if ("rated" in filters) {
+                query = { "rated": { $eq: filters['rated'] } }
+            }
         }
 
-        let cursor 
-        try{
-			cursor = await movies
-					.find(query)
-					.limit(moviesPerPage)
-					.skip(moviesPerPage * page)           
+        let cursor
+        try {
+            cursor = await movies
+                .find(query)
+                .limit(moviesPerPage)
+                .skip(moviesPerPage * page)
             const moviesList = await cursor.toArray()
             const totalNumMovies = await movies.countDocuments(query)
-            return {moviesList, totalNumMovies}
+            return { moviesList, totalNumMovies }
         }
-        catch(e){
+        catch (e) {
             console.error(`Unable to issue find command, ${e}`)
-            return { moviesList: [], totalNumMovies: 0}
+            return { moviesList: [], totalNumMovies: 0 }
         }
     }
 
-    static async getRatings(){
+    static async getRatings() {
         let ratings = []
-        try{
-            ratings = await movies.distinct("rated") 
+        try {
+            ratings = await movies.distinct("rated")
             return ratings
         }
-        catch(e){
+        catch (e) {
             console.error(`unable to get ratings, $(e)`)
             return ratings
         }
