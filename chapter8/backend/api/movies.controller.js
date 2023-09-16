b4defbe358ff0fc48d01af3b3a0c8ae0f8d41cde
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-09-16 09:39:26 +0200

    update

--- chapter8/backend/api/movies.controller.js
+++ chapter8/backend/api/movies.controller.js
@@ -1,39 +1,39 @@
-import MoviesDAO from '../dao/moviesDAO.js' 
+import MoviesDAO from '../dao/moviesDAO.js'
 
-export default class MoviesController{
+export default class MoviesController {
 
     //-- when apiGetMovies is called via a URL, there will be a query string in the response object (req.query)
     //   where certain filter parameters might be specified and passed in through key-value pairs
-    static async apiGetMovies(req,res,next){
-        
+    static async apiGetMovies(req, res, next) {
+
         // we check if moviesPerPage exists, parse it into an integer
         // we do the same for the page query string
         const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage) : 20
         const page = req.query.page ? parseInt(req.query.page) : 0
 
         // we then start with an empty filters object
-        let filters = {} 
+        let filters = {}
         // we then check if the rated query string exists, then add to the filters object
         // We do the same for title
-        if(req.query.rated){            
+        if (req.query.rated) {
             filters.rated = req.query.rated
-        } 
-        else if(req.query.title){            
-            filters.title = req.query.title            
+        }
+        else if (req.query.title) {
+            filters.title = req.query.title
         }
 
         // we next call getMovies in MoviesDAO that we have just implemented
         // getMovies will return moviesList and totalNumMovies
-        const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({filters, page, moviesPerPage})
+        const { moviesList, totalNumMovies } = await MoviesDAO.getMovies({ filters, page, moviesPerPage })
 
-        let response ={
+        let response = {
             movies: moviesList,
             page: page,
             filters: filters,
             entries_per_page: moviesPerPage,
             total_results: totalNumMovies,
         }
-        res.json(response) 
+        res.json(response)
     }
 
     // static async apiGetMovieById(req,res, next){
