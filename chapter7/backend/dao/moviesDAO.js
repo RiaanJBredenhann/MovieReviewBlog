43070681f3f855be3b9ab56c70367a769c8a13de
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-09-16 09:35:38 +0200

    update

--- chapter7/backend/dao/moviesDAO.js
+++ chapter7/backend/dao/moviesDAO.js
@@ -1,87 +1,97 @@
 import mongodb from "mongodb"
 const ObjectId = mongodb.ObjectID
 
-let movies 
+// movies stores the reference to the database
+let movies
 
-export default class MoviesDAO{ 
-    static async injectDB(conn){ 
-        if(movies){ 
+//-- we then export the class MoviesDAO which contains an async method injectDB
+//   injectDB is called as soon as the server starts and provides the database reference to movies --//
+export default class MoviesDAO {
+    static async injectDB(conn) {
+        // if there already is a reference we return
+        if (movies) {
             return
         }
-        try{ 
+        // else, we connect to the database name and movies collection
+        try {
             movies = await conn.db(process.env.MOVIEREVIEWS_NS)
-					.collection('movies')
-        } 
-        catch(e){
+                .collection('movies')
+        }
+        // if we fail, we send an error message to the console
+        catch (e) {
             console.error(`unable to connect in MoviesDAO: ${e}`)
         }
     }
 
-    static async getMovieById(id){        
-        try{                                                 
-            return await movies.aggregate([
-                {
-                    $match: {
-                        _id: new ObjectId(id),
-                    }
-                }    ,
-                { $lookup:
-                    {
-                        from: 'reviews',
-                        localField: '_id',
-                        foreignField: 'movie_id',
-                        as: 'reviews',
-                    }
-                }       
-            ]).next()            
-        }
-        catch(e){
-            console.error(`something went wrong in getMovieById: ${e}`)
-            throw e
-        }
-    }
-
-
+    //-- the getMovies method accepts a filter object as its first argument
+    //   The default filter has no filters, retrieves results at page 0 and retrieves 20 movies per page
+    //   In our app, we provide filtering results by movie title 'title' and movie rating 'rated' --//
     static async getMovies({// default filter
         filters = null,
         page = 0,
         moviesPerPage = 20, // will only get 20 movies at once
-    } = {}){
-        let query 
-        if(filters){ 
-            if("title" in filters){ 
-                query = { $text: { $search: filters['title']}}
-            }else if("rated" in filters){ 
-                query = { "rated": { $eq: filters['rated']}} 
-            }                                
+    } = {}) {
+        let query
+        if (filters) {
+            if ("title" in filters) {
+                query = { $text: { $search: filters['title'] } }
+            } else if ("rated" in filters) {
+                query = { "rated": { $eq: filters['rated'] } }
+            }
         }
 
-        let cursor 
-        try{
-			cursor = await movies
-					.find(query)
-					.limit(moviesPerPage)
-					.skip(moviesPerPage * page)           
+        //-- we then find all movies that fit our query and assign it to a cursor
+        //   if there is any error, we just return an empty moviesList and totalNumMovies to be 0 --//
+        let cursor
+        try {
+            cursor = await movies
+                .find(query)
+                .limit(moviesPerPage)
+                .skip(moviesPerPage * page)
             const moviesList = await cursor.toArray()
             const totalNumMovies = await movies.countDocuments(query)
-            return {moviesList, totalNumMovies}
+            return { moviesList, totalNumMovies }
         }
-        catch(e){
+        catch (e) {
             console.error(`Unable to issue find command, ${e}`)
-            return { moviesList: [], totalNumMovies: 0}
+            return { moviesList: [], totalNumMovies: 0 }
         }
     }
 
-    static async getRatings(){
-        let ratings = []
-        try{
-            ratings = await movies.distinct("rated") 
-            return ratings
-        }
-        catch(e){
-            console.error(`unable to get ratings, $(e)`)
-            return ratings
-        }
-    }
+    // static async getMovieById(id){        
+    //     try{                                                 
+    //         return await movies.aggregate([
+    //             {
+    //                 $match: {
+    //                     _id: new ObjectId(id),
+    //                 }
+    //             }    ,
+    //             { $lookup:
+    //                 {
+    //                     from: 'reviews',
+    //                     localField: '_id',
+    //                     foreignField: 'movie_id',
+    //                     as: 'reviews',
+    //                 }
+    //             }       
+    //         ]).next()            
+    //     }
+    //     catch(e){
+    //         console.error(`something went wrong in getMovieById: ${e}`)
+    //         throw e
+    //     }
+    // }
+
+    // static async getRatings(){
+    //     let ratings = []
+    //     try{
+    //         ratings = await movies.distinct("rated") 
+    //         return ratings
+    //     }
+    //     catch(e){
+    //         console.error(`unable to get ratings, $(e)`)
+    //         return ratings
+    //     }
+    // }
 
 }
--- chapter7/backend/index.js
+++ chapter7/backend/index.js
@@ -1,31 +1,32 @@
 import app from './server.js'
 import mongodb from "mongodb"
-import dotenv from "dotenv" 
-import MoviesDAO from './dao/moviesDAO.js' 
-import ReviewsDAO from './dao/reviewsDAO.js' 
+import dotenv from "dotenv"
+import MoviesDAO from './dao/moviesDAO.js'
+import ReviewsDAO from './dao/reviewsDAO.js'
 
-async function main(){        
- 
-    dotenv.config()    
+async function main() {
+
+    dotenv.config()
     const client = new mongodb.MongoClient(process.env.MOVIEREVIEWS_DB_URI)
-    const port = process.env.PORT || 8000     
- 
+    const port = process.env.PORT || 8000
+
     try {
         // Connect to the MongoDB cluster
-       await client.connect()
-        
-    //    await MoviesDAO.injectDB(client)
-        
-    //    await ReviewsDAO.injectDB(client)
+        await client.connect()
+        // right after connecting to the database and just before we start the server,
+        // we call injectDB to get our initial reference to the movies collection in the database
+        await MoviesDAO.injectDB(client)
 
-        app.listen(port, () =>{
-            console.log('server is running on port:'+port);        
+        //    await ReviewsDAO.injectDB(client)
+
+        app.listen(port, () => {
+            console.log('server is running on port:' + port);
         })
 
     } catch (e) {
-        console.error(e);        
+        console.error(e);
         process.exit(1)
-    } 
+    }
 }
 
 main().catch(console.error);
