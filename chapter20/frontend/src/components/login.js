a9a66300d3954e8c2191546550fa5bbfbbd18efa
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-09-27 11:02:29 +0200

    added comments for readability

--- chapter20/frontend/src/components/login.js
+++ chapter20/frontend/src/components/login.js
@@ -4,19 +4,26 @@ import Button from 'react-bootstrap/Button';
 
 const Login = props => {
 
+    //-- we set the initial name and id fields to be empty
     const [name, setName] = useState("");
     const [id, setId] = useState("");
 
+    //-- binds the 'name' field value to the 'name' state variable
     const onChangeName = e => {
         const name = e.target.value;
         setName(name);
     }
 
+    //-- binds the 'id' field value to the 'id' state variable
     const onChangeId = e => {
         const id = e.target.value;
         setId(id);
     }
 
+    //-- this method calls 'login'
+    //   App.js has a method of the same name that is called in turn through props.login
+    //-- in other words, from the Login component, we call the login function in App.js and set App.js's 'user' state
+    //   we are then able to pass on the logged-in 'user' to other components
     const login = () => {
         props.login({
             name: name, 
